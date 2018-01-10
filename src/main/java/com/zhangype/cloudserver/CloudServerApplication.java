3b7cb48faabd4a31323ef22ffed4a484884fa4c7
zhangype <zhangype@yonyou.com>
2018-01-10 16:05:58 +0800

    初始化项目

---
+++ src/main/java/com/zhangype/cloudserver/CloudServerApplication.java
@@ -0,0 +1,22 @@
+package com.zhangype.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心启动类
+ *
+ * @author zhangype@yonyou.com
+ * @version V1.0.0
+ * @date 2018/1/9
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerApplication.class, args);
+    }
+
+}
